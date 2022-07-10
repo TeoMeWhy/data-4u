115d95edf2aa2730aa96d5317c0853a5c5b4c22f
teocalvo2@gmail.com <teocalvo2@gmail.com>
2022-07-10 13:35:59 +0000

    Schemas criados

---
+++ databases/create.sql
@@ -0,0 +1,5 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS bronze_censo_escolar LOCATION "/mnt/datalake/bronze/censo_escolar";
+CREATE DATABASE IF NOT EXISTS bronze_enem LOCATION "/mnt/datalake/bronze/enem";
+CREATE DATABASE IF NOT EXISTS bronze_gc LOCATION "/mnt/datalake/bronze/gc";
+CREATE DATABASE IF NOT EXISTS bronze_olist LOCATION "/mnt/datalake/bronze/olist";
