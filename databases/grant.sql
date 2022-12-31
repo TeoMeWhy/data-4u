5863557280fec0003a90f57f9811472cf8a22545
teocalvo2@gmail.com <teocalvo2@gmail.com>
2022-12-31 17:44:04 +0000

    Grant for twitch group

--- databases/grant.sql
+++ databases/grant.sql
@@ -1,45 +1,45 @@
 -- Databricks notebook source
 -- DBTITLE 1,Censo Escolar
-GRANT USAGE ON DATABASE `bronze_censo_escolar` TO `apoiadores`;
-GRANT SELECT ON DATABASE `bronze_censo_escolar` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `bronze_censo_escolar` TO `apoiadores`;
+GRANT USAGE ON DATABASE `bronze_censo_escolar` TO `twitch`;
+GRANT SELECT ON DATABASE `bronze_censo_escolar` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `bronze_censo_escolar` TO `twitch`;
 
 -- COMMAND ----------
 
 -- DBTITLE 1,ENEM
-GRANT USAGE ON DATABASE `bronze_enem` TO `apoiadores`;
-GRANT SELECT ON DATABASE `bronze_enem` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `bronze_enem` TO `apoiadores`;
+GRANT USAGE ON DATABASE `bronze_enem` TO `twitch`;
+GRANT SELECT ON DATABASE `bronze_enem` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `bronze_enem` TO `twitch`;
 
 -- COMMAND ----------
 
 -- DBTITLE 1,Gamers Club
-GRANT USAGE ON DATABASE `bronze_gc` TO `apoiadores`;
-GRANT SELECT ON DATABASE `bronze_gc` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `bronze_gc` TO `apoiadores`;
+GRANT USAGE ON DATABASE `bronze_gc` TO `twitch`;
+GRANT SELECT ON DATABASE `bronze_gc` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `bronze_gc` TO `twitch`;
 
-GRANT USAGE ON DATABASE `silver_gc` TO `apoiadores`;
-GRANT SELECT ON DATABASE `silver_gc` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `silver_gc` TO `apoiadores`;
+GRANT USAGE ON DATABASE `silver_gc` TO `twitch`;
+GRANT SELECT ON DATABASE `silver_gc` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `silver_gc` TO `twitch`;
 
 -- COMMAND ----------
 
 -- DBTITLE 1,Olist
-GRANT USAGE ON DATABASE `bronze_olist` TO `apoiadores`;
-GRANT SELECT ON DATABASE `bronze_olist` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `bronze_olist` TO `apoiadores`;
+GRANT USAGE ON DATABASE `bronze_olist` TO `twitch`;
+GRANT SELECT ON DATABASE `bronze_olist` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `bronze_olist` TO `twitch`;
 
-GRANT USAGE ON DATABASE `silver_olist` TO `apoiadores`;
-GRANT SELECT ON DATABASE `silver_olist` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `silver_olist` TO `apoiadores`;
+GRANT USAGE ON DATABASE `silver_olist` TO `twitch`;
+GRANT SELECT ON DATABASE `silver_olist` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `silver_olist` TO `twitch`;
 
 -- COMMAND ----------
 
 -- DBTITLE 1,TSE
-GRANT USAGE ON DATABASE `bronze_tse` TO `apoiadores`;
-GRANT SELECT ON DATABASE `bronze_tse` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `bronze_tse` TO `apoiadores`;
+GRANT USAGE ON DATABASE `bronze_tse` TO `twitch`;
+GRANT SELECT ON DATABASE `bronze_tse` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `bronze_tse` TO `twitch`;
 
-GRANT USAGE ON DATABASE `silver_tse` TO `apoiadores`;
-GRANT SELECT ON DATABASE `silver_tse` TO `apoiadores`;
-GRANT READ_METADATA ON DATABASE `silver_tse` TO `apoiadores`;
+GRANT USAGE ON DATABASE `silver_tse` TO `twitch`;
+GRANT SELECT ON DATABASE `silver_tse` TO `twitch`;
+GRANT READ_METADATA ON DATABASE `silver_tse` TO `twitch`;
