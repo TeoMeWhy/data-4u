ba2245df1cce2236f079c404f1cbb8736646c67f
teocalvo2@gmail.com <teocalvo2@gmail.com>
2023-05-10 19:01:23 +0000

    Ingestao full-load + cdc

--- databases/create.sql
+++ databases/create.sql
@@ -13,3 +13,9 @@ CREATE DATABASE IF NOT EXISTS silver_tse LOCATION "/mnt/datalake/silver/tse";
 -- DBTITLE 1,Olist
 CREATE DATABASE IF NOT EXISTS bronze.olist;
 CREATE DATABASE IF NOT EXISTS silver.olist;
+
+-- COMMAND ----------
+
+-- DBTITLE 1,IGDB
+CREATE DATABASE IF NOT EXISTS bronze.igdb;
+CREATE DATABASE IF NOT EXISTS silver.igdb;
