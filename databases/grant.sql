5942d174062e710ccf2f68d727a3954a36c8a907
Téo Calvo <teocalvo2@gmail.com>
2023-05-19 16:32:54 -0300

    Reestruturacao de pastas

--- databases/grant.sql
+++ databases/grant.sql
@@ -1,26 +1,4 @@
 -- Databricks notebook source
--- DBTITLE 1,Censo Escolar
-GRANT USAGE ON DATABASE `bronze_censo_escolar` TO `twitch`;
-GRANT SELECT ON DATABASE `bronze_censo_escolar` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `bronze_censo_escolar` TO `twitch`;
-
--- COMMAND ----------
-
--- DBTITLE 1,ENEM
-GRANT USAGE ON DATABASE `bronze_enem` TO `twitch`;
-GRANT SELECT ON DATABASE `bronze_enem` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `bronze_enem` TO `twitch`;
-
--- COMMAND ----------
-
--- DBTITLE 1,Gamers Club
-GRANT USAGE ON DATABASE `bronze_gc` TO `twitch`;
-GRANT SELECT ON DATABASE `bronze_gc` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `bronze_gc` TO `twitch`;
-
-GRANT USAGE ON DATABASE `silver_gc` TO `twitch`;
-GRANT SELECT ON DATABASE `silver_gc` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `silver_gc` TO `twitch`;
 
 -- COMMAND ----------
 
@@ -33,13 +11,3 @@ GRANT USAGE ON DATABASE `silver.olist` TO `twitch`;
 GRANT SELECT ON DATABASE `silver.olist` TO `twitch`;
 GRANT READ_METADATA ON DATABASE `silver.olist` TO `twitch`;
 
--- COMMAND ----------
-
--- DBTITLE 1,TSE
-GRANT USAGE ON DATABASE `bronze_tse` TO `twitch`;
-GRANT SELECT ON DATABASE `bronze_tse` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `bronze_tse` TO `twitch`;
-
-GRANT USAGE ON DATABASE `silver_tse` TO `twitch`;
-GRANT SELECT ON DATABASE `silver_tse` TO `twitch`;
-GRANT READ_METADATA ON DATABASE `silver_tse` TO `twitch`;
