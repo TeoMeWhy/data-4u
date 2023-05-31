e48e13d9709d4be1a15ac9f682e88b5231e99fff
Téo Calvo <teocalvo2@gmail.com>
2023-05-31 09:19:56 -0300

    IGDB raw

---
+++ src/01.raw/igdb/execute.sh
@@ -0,0 +1,17 @@
+conda activate gamelake;
+
+python main.py --endpoint characters --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint collections --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint companies --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint franchises --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint games --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint game_modes --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint game_versions --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint genres --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint languages --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint language_supports --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint platforms --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint platform_families --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint platform_versions --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint regions --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
+python main.py --endpoint themes --mode all --delay 4500 --n_jobs 8 --profile_aws teomewhy;
