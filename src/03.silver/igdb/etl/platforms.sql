f402b081d2ae98aebd45ecea071dd165d6572370
teocalvo2@gmail.com <teocalvo2@gmail.com>
2023-06-02 12:54:15 +0000

    silver_platforms job

--- src/03.silver/igdb/etl/platforms.sql
+++ src/03.silver/igdb/etl/platforms.sql
@@ -1,13 +1,13 @@
 SELECT abbreviation AS descAbbreviation,
        alternative_name AS descAlternativeName,
        CASE
-          WHEN category = 1 THEN 'console'
-          WHEN category = 2 THEN 'arcade'
-          WHEN category = 3 THEN 'platform'
-          WHEN category = 4 THEN 'operating_system'
-          WHEN category = 5 THEN 'portable_console'
-          WHEN category = 6 THEN 'computer'
-          ELSE 'missing category'
+           WHEN category = 1 THEN 'console'
+           WHEN category = 2 THEN 'arcade'
+           WHEN category = 3 THEN 'platform'
+           WHEN category = 4 THEN 'operating_system'
+           WHEN category = 5 THEN 'portable_console'
+           WHEN category = 6 THEN 'computer'
+           ELSE 'missing category'
        END AS descCategory,
        from_unixtime(created_at) AS dtCreated,
        generation AS nrGeneration,
