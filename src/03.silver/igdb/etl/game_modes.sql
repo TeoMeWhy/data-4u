56b8176749e02b7733ca4f0c91503506127f4382
teocalvo2@gmail.com <teocalvo2@gmail.com>
2023-06-06 13:10:52 +0000

    Game modes ETL + Workflows

---
+++ src/03.silver/igdb/etl/game_modes.sql
@@ -0,0 +1,7 @@
+SELECT id AS idGameMode,
+       name AS descName,
+       slug AS descSlug,
+       from_unixtime(created_at) AS dtCreated,
+       from_unixtime(updated_at) AS dtUpdated
+
+FROM bronze.igdb.game_modes
