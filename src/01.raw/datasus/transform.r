5011326bcdfe3842ae89c6af70dc97f8a4e6c68e
TeoCalvo <TeoCalvo>
2023-08-16 22:00:58 +0000

    Trabalho pela metade em datasus sinasc
    Terminar a fase de bronze dep[ois te realizado os ajustes de raw

--- src/01.raw/datasus/transform.r
+++ src/01.raw/datasus/transform.r
@@ -9,42 +9,55 @@ library(read.dbc)
 library(foreach)
 library(doParallel)
 library(jsonlite)
+library(SparkR)
 
 date = format(Sys.time(), "%Y%m%d")
 
-datasource <- dbutils.widgets.get("datasource")
+# datasource <- dbutils.widgets.get("datasource")
+datasource <- 'sinasc'
 datasources <- fromJSON("datasources.json")
 
 path = datasources[datasource][[1]]['target'][[1]]
 partes <- unlist(strsplit(path, "/"))
 partes <- partes[-length(partes)]
 dbc_folder <- paste(partes, collapse = "/")
-csv_folder <- sub('/dbc/landing', '/csv', dbc_folder)
+parquet_folder <- sub('/dbc/landing', '/parquet/', dbc_folder)
+parquet_folder <- sub('/dbfs', '', parquet_folder)
 
 files <- list.files(dbc_folder, full.names=TRUE)
 
 # COMMAND ----------
 
 etl <- function(f) {
-    print(f)
-    df= read.dbc(f)
-    lista = strsplit(f, "/")[[1]]
-    file = gsub(".dbc", paste(date, "csv", sep="."), lista[length(lista)])
-    write.csv2(df, paste(csv_folder, file, sep="/"), row.names=FALSE)
+    df <- createDataFrame(read.dbc(f))
+    write.parquet(df, parquet_folder, mode='append')
     file.rename(from=f, to=gsub("landing", "proceeded", f))
     return
 }
 
 # COMMAND ----------
 
-registerDoParallel(8)
-while (sum(is.na(files)) != length(files)) {
-  batch = files[1:min(8, length(files))]
-  files = files[1+min(8, length(files)):length(files)]
-  foreach (i=batch) %dopar% {
-    print(i)
-    if (is.na(i) == FALSE) {
-      etl(i)
-    }
-  }
+for (i in files){
+  print(i)
+  etl(i)
 }
+
+# COMMAND ----------
+
+# registerDoParallel(8)
+# while (sum(is.na(files)) != length(files)) {
+#   batch = files[1:min(8, length(files))]
+#   files = files[1+min(8, length(files)):length(files)]
+#   foreach (i=batch) %dopar% {
+#     print(i)
+#     if (is.na(i) == FALSE) {
+#       etl(i)
+#     }
+#   }
+# }
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC
+# MAGIC dbutils.fs.mv("/mnt/datalake/datasus/sinasc/dbc/landind/", "/mnt/datalake/datasus/sinasc/dbc/landing/", True)
--- src/02.bronze/datasus/ingestao.py
+++ src/02.bronze/datasus/ingestao.py
@@ -56,3 +56,42 @@ stream.awaitTermination()
 
 table = delta.DeltaTable.forName(spark, f"{database_name}.{table_name}")
 table.vacuum()
+
+# COMMAND ----------
+
+df = spark.read.parquet("/mnt/datalake/datasus/sinasc/parquet/")
+
+print("Total de linhas:", df.count())
+print("Total de cagadas:", df.filter('len(DTNASC) < 8').count())
+
+# df.createOrReplaceTempView('sinasc')
+
+df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("sandbox.twitch.sinasc")
+
+df.createOrReplaceTempView("sinasc")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select count(*) as tdeLinhas,
+# MAGIC         count(distinct
+# MAGIC                 coalesce(contador, 'NA') ||
+# MAGIC                 coalesce(CODESTAB, 'NA') ||
+# MAGIC                 coalesce(CODMUNNASC, 'NA') ||
+# MAGIC                 coalesce(NUMEROLOTE, 'NA') ||
+# MAGIC                 coalesce(DTCADASTRO, 'NA') ||
+# MAGIC                 coalesce(DTNASC, 'NA') ||
+# MAGIC                 coalesce(HORANASC, 'NA') ||
+# MAGIC                 coalesce(DTNASCMAE, 'NA')
+# MAGIC         ) as qtdeLinahsDst,
+# MAGIC
+# MAGIC         tdeLinhas - qtdeLinahsDst
+# MAGIC
+# MAGIC from sandbox.twitch.sinasc
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from sandbox.twitch.sinasc
+# MAGIC limit 10
