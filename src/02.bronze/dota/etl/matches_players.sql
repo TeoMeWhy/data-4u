1daba84a4c39dbebc80c3210ba1b49c34c64b6dd
TeoCalvo <TeoCalvo>
2023-11-25 23:31:02 +0000

    fix query

--- src/02.bronze/dota/etl/matches_players.sql
+++ src/02.bronze/dota/etl/matches_players.sql
@@ -1,10 +1,11 @@
 WITH tb_explode AS (
 
-  SELECT explode(players) as players
+  SELECT players.*
   FROM {table}
+  LATERAL VIEW explode(players) FROM {table} AS players
 
 )
 
-SELECT players.*
+SELECT *
 FROM tb_explode
 QUALIFY row_number() OVER (PARTITION BY account_id, match_id ORDER BY start_time DESC) = 1
